33445a903256e85b1dd7aa3de90785ab60c4298d
bKlueber <bradyklueber@gmail.com>
2025-04-18 04:06:26 -0700

    Started setting up generic items
    So im setting up the generic item classes. I decided  we will need two .txt, one for a master list of items and one for player inventory. We can use array lists for the data structure. Having the master item list will make spawning items into the world much easier. We can just call one by element id then assign a quantity vs having to create duplicate items manually each time.

--- PrvAck.java
+++ PrvAck.java
@@ -1,4 +1,24 @@
 public class PrvAck{
-
+//right now nothing to add here
     
 } 
+
+class GenericItem{ //this is the class that is used for every single item in the game. See items.txt for more information
+
+    String itemName;
+    String itemDescription;
+    double itemValue;
+    double itemWeight; //not neccessary but in my (Brady) opinion i think it adds depth to our systems
+    int baseDamage; //will not be applicable to every item but by defining it here, and then assigning it in the txt we will save a lot of time and effort
+    int baseArmor; //same as baseDamage, as in may be null for some items but will make adjusting and balancing worlds easier
+    
+        public GenericItem genericItem (String  name, String description, double value, double weight, int damage, int armor) {  //this is matching the paremeter to instances of each variable
+            this.itemName = name;
+            this.itemDescription = description;
+            this.itemValue = value;
+            this.itemWeight = weight;
+            this.baseDamage = damage;
+            this.baseArmor = armor;
+        }
+
+}
