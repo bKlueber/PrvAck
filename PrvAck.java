cf08493f1d50dbc2b28b9811de383919b7523e78
bKlueber <bradyklueber@gmail.com>
2025-04-19 15:47:00 -0700

    Small updates
    Just plugging away on week 1-3 from the proposal still. No major roadblocks but have came up with questions for team that will be in the discord

--- PrvAck.java
+++ PrvAck.java
@@ -103,6 +103,13 @@ class DataIO {//this classs calls the corrosponding txt files, parses and reads 
 
 class PlayerValues{ //this classs is going to control player base stats, including health, invnetory sizee, base damage (from strength rolls, for example)
 
+    String playerName;
+    int playerHealth;
+    PlayerClasses selectedClass;
+    BuildPlayerRolls playerBaseStats;
+
+
+
 
 }
 
@@ -111,7 +118,45 @@ class BuildPlayerRolls { //this is going to be for when the player starts the ga
 }
 
 class PlayerClasses { /*i was thinking keeep it  simple with just three classes, hacking/tech, strength/tank/meleee, and stealth/rranged, was cosidering having unique ability for each
-                        but don't know if i should make class effect base stats? Maybe thats a stretch goal. Was planning on using bolean check and then looping to give access to unique skills per class
+                        but don't know if i should make class effect base stats? Maybe thats a stretch goal. Was planning on using bolean check and then looping to give access to unique
+                        skills per class*/
+    StrengthBuild userChoseSTR;
+    DexterityBuild userChoseDEX;
+    IntelligenceBuild useChoseINTL;
 
 }
+
+class diceStatRoller {
+
+   String playerVitality = null; //these are just place holders
+   String playerStrength = null; //need to research how to make it so the player can like roll for these or adjust base stats constrained by class, need to consult story team and then come back
+
+}
+
+class StrengthBuild {
+    
+    int baseDamageSTR;
+    double baseCarryWeightSTR;
+    int baseHealthSTR;
+    boolean specialAbilitySTR; 
+
+}
+
+class DexterityBuild{ 
+    
+    int baseDamageDEX;
+    double baseCarryWeightDEX;
+    int baseHealthDEX;
+    boolean specialAbilityDEX; 
+}
+
+class IntelligenceBuild{
+
+    int baseDamageINTL;
+    double baseCarryWeightINTL;
+    int baseHealthINTL;
+    boolean specialAbilityINTL; 
+
+}
+
 
