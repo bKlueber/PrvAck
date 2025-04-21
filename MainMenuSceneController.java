6241bc35648af2847391f3a3d24715375ef242c2
Zhalllberg98 <135420444+Zhalllberg98@users.noreply.github.com>
2025-04-21 13:55:05 -0700

    Update MainMenuSceneController.java with code from the scene builder

--- MainMenuSceneController.java
+++ MainMenuSceneController.java
@@ -1,3 +1,25 @@
-public class MainSceneController {
-    
+import javafx.event.ActionEvent;
+
+public class MainMenuSceneController {
+
+    @FXML
+    void btnContinueGameClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoadGameClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewGameClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettingsClicked(ActionEvent event) {
+
+    }
+
 }
