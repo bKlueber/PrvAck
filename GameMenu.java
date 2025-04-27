051ebafb01b22bcc810d38f9f4b37d36edf034df
Zhalllberg98 <135420444+Zhalllberg98@users.noreply.github.com>
2025-04-27 15:23:35 -0700

    Updated the main menu GUI to play background music

--- GameMenu.java
+++ GameMenu.java
@@ -3,6 +3,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 public class MainMenuApp extends Application {
 
@@ -13,8 +16,21 @@ public class MainMenuApp extends Application {
             primaryStage.setTitle("Main Menu");
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
+
+             // Setup Background Music
+            String musicFile = "background.mp3"; // Make sure this file is in the right place
+            Media sound = new Media(new File(musicFile).toURI().toString());
+            mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); // Loop the music
+            mediaPlayer.play();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+     @Override
+     public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
         }
     }
 
