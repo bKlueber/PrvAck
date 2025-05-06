c447ce5d75ec30039dc5dccb1bcfcdf08fd32055
bKlueber <bradyklueber@gmail.com>
2025-05-06 10:26:31 -0700

    Start of the combat system
    just started a file for the combat system, I think it makes sense to keep it in its own file but if anyone disagrees I am open to changing the structure!

---
+++ CombatSystem.java
@@ -0,0 +1,9 @@
+public class CombatSystem { //the beggining of something great
+
+    public void combatEngaged(PlayerValues player, GenericNPC enemy) { 
+        /*need to watch some youtube videos on how to create basic combat system, I have some ideas but i want  to make sure im coming at 
+        this task the right way */
+
+    }
+
+}
